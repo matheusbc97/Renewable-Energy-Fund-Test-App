647f571c7edfc75cf6f1463869958828a024f3ff
Matheus Botelho <matheusbcdev@gmail.com>
2023-03-26 19:33:34 -0300

    Update AppNavigator.tsx

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -3,16 +3,8 @@ import {
   NavigationContainer,
   Theme,
 } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {
-  FundDetailsScreen,
-  HomeScreen,
-  LoginScreen,
-  RegisterScreen,
-} from '../screens';
-
-import {StackParams} from './types/StackParams';
+import StackNavigator from './navigators/StackNavigator';
 
 const AppTheme: Theme = {
   ...DefaultTheme,
@@ -22,23 +14,10 @@ const AppTheme: Theme = {
   },
 };
 
-const Stack = createNativeStackNavigator<StackParams>();
-
 function AppNavigator() {
   return (
     <NavigationContainer theme={AppTheme}>
-      <Stack.Navigator
-        initialRouteName="FundDetails"
-        screenOptions={{title: ''}}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="FundDetails" component={FundDetailsScreen} />
-      </Stack.Navigator>
+      <StackNavigator />
     </NavigationContainer>
   );
 }
