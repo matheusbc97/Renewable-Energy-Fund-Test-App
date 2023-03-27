26d07c64026936933b78f97adf5b4fabf32c99f0
Matheus Botelho <matheusbcdev@gmail.com>
2023-03-27 00:06:46 -0300

    refactor: FundBreakDown styles extraction

--- src/screens/FundDetailsScreen/components/FundBreakDown.tsx
+++ src/screens/FundDetailsScreen/components/FundBreakDown.tsx
@@ -1,12 +1,51 @@
-import {FlatList, ScrollView, View} from 'react-native';
+import {useState} from 'react';
+import {FlatList, TouchableOpacity, View} from 'react-native';
 import styled from 'styled-components/native';
 
 import {Text, TextLink} from '../../../components';
 import {COLORS} from '../../../constants/colors';
 
-const FundBreakdownSectionTitle = styled(Text)`
-  flex-direction: row;
+const FundBreakdownSectionTitle = styled(Text)``;
+
+const Container = styled.View`
+  margin-top: 50px;
+`;
+
+const Title = styled(Text)`
+  padding-horizontal: 20px;
+`;
+
+const LabelsScrollView = styled.ScrollView`
+  margin-top: 20px;
   margin-horizontal: 10px;
+`;
+
+const FundBreakdownFlatList = styled(FlatList)`
+  margin-top: 20px;
+  margin-horizontal: 10px;
+`;
+
+const ReadMoreTextLink = styled(TextLink)`
+  padding-bottom: 23px;
+  padding-horizontal: 10px;
+`;
+
+const FundBreakdownListItemContainer = styled.View`
+  margin-horizontal: 10px;
+  border-width: 1px;
+  border-color: ${COLORS.border};
+  border-radius: 3px;
+  height: 280px;
+  width: 220px;
+`;
+
+const FundBreakdownListItemContent = styled.View`
+  padding: 10px;
+  flex: 1;
+`;
+
+const FundBreakdownListItemDescriptionText = styled(Text)`
+  margin-top: 10px;
 `;
 
 const fundBreakdownSectionTitles = [
@@ -17,57 +56,58 @@ const fundBreakdownSectionTitles = [
 ];
 
 export default function FundBreakDown() {
+  const [labelSelectedIndex, setLabelSelectedIndex] = useState(0);
+
   return (
-    <View style={{marginTop: 50}}>
-      <Text
-        variant="title"
-        colorVariant="onBackground"
-        style={{paddingHorizontal: 20}}>
+    <Container>
+      <Title variant="title" colorVariant="onBackground">
         Fund Breakdown
-      </Text>
+      </Title>
 
-      <ScrollView
-        horizontal
-        style={{flexDirection: 'row', marginTop: 20, marginHorizontal: 10}}>
-        {fundBreakdownSectionTitles.map((title, index) => (
-          <FundBreakdownSectionTitle key={title}>
-            {title}
-          </FundBreakdownSectionTitle>
-        ))}
-      </ScrollView>
+      <LabelsScrollView horizontal>
+        {fundBreakdownSectionTitles.map((title, index) => {
+          const isSelected = index === labelSelectedIndex;
 
-      <FlatList
+          return (
+            <TouchableOpacity
+              onPress={() => setLabelSelectedIndex(index)}
+              style={{
+                borderBottomWidth: 1,
+                borderBottomColor: isSelected ? COLORS.primary : 'transparent',
+                paddingBottom: 5,
+                marginHorizontal: 10,
+              }}>
+              <FundBreakdownSectionTitle
+                key={title}
+                colorVariant={
+                  isSelected ? 'onBackground' : 'onBackgroundVariant'
+                }>
+                {title}
+              </FundBreakdownSectionTitle>
+            </TouchableOpacity>
+          );
+        })}
+      </LabelsScrollView>
+
+      <FundBreakdownFlatList
         data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
         horizontal
-        style={{marginTop: 20, marginHorizontal: 10}}
         renderItem={({item}) => (
-          <View
-            style={{
-              marginHorizontal: 10,
-              borderWidth: 1,
-              borderColor: COLORS.border,
-              borderRadius: 3,
-              height: 280,
-              width: 220,
-            }}>
+          <FundBreakdownListItemContainer>
             <View style={{height: 100}} />
 
-            <View style={{padding: 10, flex: 1}}>
+            <FundBreakdownListItemContent>
               <Text>AspiraDAC</Text>
-              <Text style={{marginTop: 10}}>
+              <FundBreakdownListItemDescriptionText>
                 Aspira is building a modular, direct air capture system with the
                 energy supply integrated into the modules. Read more
-              </Text>
-            </View>
+              </FundBreakdownListItemDescriptionText>
+            </FundBreakdownListItemContent>
 
-            <TextLink
-              style={{paddingBottom: 23, paddingHorizontal: 10}}
-              onPress={() => {}}>
-              Read More
-            </TextLink>
-          </View>
+            <ReadMoreTextLink onPress={() => {}}>Read More</ReadMoreTextLink>
+          </FundBreakdownListItemContainer>
         )}
       />
-    </View>
+    </Container>
   );
 }
