019b35abe464156081ba009d6145f3c00dd2fd6f
Matheus Botelho <matheusbcdev@gmail.com>
2023-03-28 16:05:16 -0300

    refactor: StackNavigator code clean

--- src/navigation/navigators/StackNavigator/index.tsx
+++ src/navigation/navigators/StackNavigator/index.tsx
@@ -1,76 +1,16 @@
-import {useNavigation} from '@react-navigation/native';
-import {
-  createNativeStackNavigator,
-  NativeStackHeaderProps,
-} from '@react-navigation/native-stack';
-import {TouchableOpacity, View} from 'react-native';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {SvgIcon, Text} from '../../../components';
-import {COLORS} from '../../../constants/colors';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import {FundDetailsScreen, LoginScreen, RegisterScreen} from '../../../screens';
 import BottomTabsNavigator from '../BottomTabsNavigator';
+import Header from './components/Header';
 
 import {StackParams} from './types/StackParams';
 
-interface RenderLeftIconProps {
-  tintColor?: string;
-  pressColor?: string;
-  pressOpacity?: number;
-  labelVisible?: boolean;
-}
-
-const LeftIcon = ({}: RenderLeftIconProps) => {
-  const navigation = useNavigation();
-
-  if (navigation.canGoBack() === false) {
-    return null;
-  }
-  return (
-    <TouchableOpacity onPress={() => navigation.goBack()}>
-      <SvgIcon name="ArrowLeft" color={COLORS.onBackground} />
-    </TouchableOpacity>
-  );
-};
-
 const Stack = createNativeStackNavigator<StackParams>();
-
-function Header({options, navigation, back}: NativeStackHeaderProps) {
-  const insetsTop = useSafeAreaInsets().top;
-  return (
-    <View
-      style={{
-        paddingTop: insetsTop,
-        paddingHorizontal: 25,
-        flexDirection: 'row',
-        alignItems: 'center',
-        backgroundColor: COLORS.background,
-        borderBottomWidth: 1,
-        borderBottomColor: COLORS.border,
-        height: 60 + insetsTop,
-      }}>
-      {back && (
-        <View style={{flex: 1}}>
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <SvgIcon name="ArrowLeft" color={COLORS.onBackground} />
-          </TouchableOpacity>
-        </View>
-      )}
-
-      <View style={{alignItems: 'center'}}>
-        <Text variant="title">{options.title}</Text>
-        <Text colorVariant="onBackgroundVariant">{options.subtitle}</Text>
-      </View>
-
-      <View style={{flex: 1}} />
-    </View>
-  );
-}
 
 function StackNavigator() {
   return (
-    <Stack.Navigator
-      screenOptions={{title: '', headerLeft: LeftIcon, header: Header}}>
+    <Stack.Navigator screenOptions={{title: '', header: Header}}>
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
       <Stack.Screen
